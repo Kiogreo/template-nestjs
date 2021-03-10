380594edf1658f453109790cd7cb1c77759f34f8
Muhammad Nadzmi Bin Mohamed Idzham <nadzmiidzham@gmail.com>
2021-03-10 21:40:52 +0800

    updated stub for mongodb

--- stub/mongodb/app/health/health.controller.ts
+++ stub/mongodb/app/health/health.controller.ts
@@ -2,15 +2,16 @@ import { Controller, Get } from '@nestjs/common';
 import {
     HealthCheckService,
     HealthIndicatorResult,
-    TypeOrmHealthIndicator,
+    MongooseHealthIndicator,
 } from '@nestjs/terminus';
 import { HealthCheckResult } from '@nestjs/terminus/dist/health-check';
-import { AppLogger } from '../../common/logger/logger.service';
+import { AppLogger } from 'src/common/logger/logger.service';
+
 @Controller('health')
 export class HealthController {
     constructor(
         private readonly health: HealthCheckService,
-        private readonly typeormHealth: TypeOrmHealthIndicator,
+        private readonly mongooseHealth: MongooseHealthIndicator, // NOTE: enable this if using mongodb
         private readonly logger: AppLogger,
     ) {
         this.logger.setContext(HealthController.name);
@@ -21,7 +22,7 @@ export class HealthController {
         this.logger.info('Health check triggered');
         return this.health.check([
             (): Promise<HealthIndicatorResult> =>
-                this.typeormHealth.pingCheck('mysqldb'),
+                this.mongooseHealth.pingCheck('mongodb'),
         ]);
     }
 }
